90c4eba1c69f136b232e39548c7af0805d37d278
Raheel Arif <l080154@gmail.com>
2024-02-25 20:42:58 -0500

    adding switch type matching example

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
@@ -217,4 +218,37 @@ func checkWebsite(website string, c chan string, onComplete func()) {
 
 func niceFuncWithMultipleReturns() (size int64, status string) {
 	return 100, "Thank you for your purchase"
+}
+
+func init() {
+	http.HandleFunc("/", errorHandler(betterHandler))
+}
+
+func errorHandler(f func(http.ResponseWriter, *http.Request) error) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		err := f(w, r)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			log.Printf("handling %q: %v", r.RequestURI, err)
+		}
+	}
+}
+
+func betterHandler(w http.ResponseWriter, r *http.Request) error {
+	if err := doThis(); err != nil {
+		return fmt.Errorf("doing this: %v", err)
+	}
+
+	if err := doThat(); err != nil {
+		return fmt.Errorf("doing that: %v", err)
+	}
+	return nil
+}
+
+func doThis() error {
+	return nil
+}
+
+func doThat() error {
+	return nil
 }
